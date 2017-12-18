4237d3c35d794ef87cdcbeb60103bddad40176f4
Marina Siena <marinasiena@Marinas-MacBook-Pro.local>
2017-12-18 18:37:24 -0500

    init

---
+++ assets/javascript/logic.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+// Page transitions
+$(document).ready(function(){
+    $("#leaderboards-button").click(function(){
+        $("#about-page").hide();
+        $("#home-page").hide();
+        $("#leaderboards-page").show();
+      });
+    $("#about-button").click(function(){
+        $("#leaderboards-page").hide();
+        $("#home-page").hide();
+        $("#about-page").show();
+      });
+    $("#home-button").click(function(){
+        $("#leaderboards-page").hide();
+        $("#about-page").hide();
+        $("#home-page").show();
+      });
+    });
+  });
+
+  window.onload = function() {
+    $("#leaderboards-page").hide();
+    $("#about-page").hide();
+    $("#home-page").show();
+  };
